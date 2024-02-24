5276bfd092831cdf21b275d16897373c8be83152
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-24 13:17:34 -0600

    added print_maze

--- path.py
+++ path.py
@@ -15,9 +15,22 @@ maze = [
     ["#", "#", " ", " ", " ", " ", " ", "#", "#"]
 ]
 
+def print_maze(stdscr, maze, path=[]):
+    GREEN = curses.color_pair(1)
+    PURPLE = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2, value, GREEN)
+
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_PURPLE, curses.COLOR_BLACK)
+    #green_and_black = curses.color_pair(1)
+
+    
     stdscr.clear()
-    stdscr.addstr(0, 0, "Welcome to the maze!")
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
